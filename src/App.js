9decef7246c27cefbe1f2ac5fd3c33dc475b8673
B H An <anbyunghoon0107@gamil.com>
2021-11-04 17:15:33 +0900

    Sass

--- src/App.js
+++ src/App.js
@@ -1,24 +1,11 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import SassComponent from './SassComponent';
 
 class App extends Component {
   render() {
-      return (
-        <div className="App">
-        <header>
-          <img src={logo} className="logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-            >
-            Learn React
-          </a>
-        </header>
+    return ( 
+      <div>
+        <SassComponent />
       </div>
     );
   }
